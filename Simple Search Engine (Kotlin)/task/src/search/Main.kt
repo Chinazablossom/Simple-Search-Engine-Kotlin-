b49c68f0f91d6d9effe7f667067979b6af2eb0e0
chinazaBlossom <chinazaBlossom7@gmail.com>
2024-07-18 10:50:10 +0100

    stage 3

--- Simple Search Engine (Kotlin)/task/src/search/Main.kt
+++ Simple Search Engine (Kotlin)/task/src/search/Main.kt
@@ -1,26 +1,55 @@
 package search
 
+import kotlin.system.exitProcess
+
 fun main() {
     SimpleSearchEngine()
 }
 
 class SimpleSearchEngine {
+    val dataLines = mutableListOf<String>()
 
     init {
+        menu()
+    }
 
+    private fun menu() {
         println("Enter the number of people:")
         val numberOfPeople = readln().toInt()
-        val dataLines = mutableListOf<String>()
 
         println("Enter all people:")
         repeat(numberOfPeople) {
             dataLines.add(readln())
         }
-        println("\nEnter the number of search queries:")
-        val numOfSearchQueries = readln().toInt()
+        do {
+            println("\n=== Menu ===")
+            println(
+                "1. Find a person\n" +
+                        "2. Print all people\n" +
+                        "0. Exit"
+            )
 
-        repeat(numOfSearchQueries) {
-            println("Enter data to search people:")
+            val userInput = readln().toInt()
+            when (userInput) {
+                0 -> {
+                    println("\nBye!")
+                    exitProcess(0)
+                }
+                1 -> search()
+                2 -> displayAllPeople()
+                else -> {
+                    println("Incorrect option! Try again.")
+                }
+
+            }
+
+        } while (userInput != 0)
+
+
+    }
+
+    private fun search() {
+            println("Enter a name or email to search all suitable people.")
             val data = readln().lowercase()
             val results = dataLines.filter { it.lowercase().contains(data) }
 
@@ -30,9 +59,13 @@ class SimpleSearchEngine {
             } else {
                 println("No matching people found.")
             }
-        }
-
 
     }
+
+    private fun displayAllPeople() {
+        println("\n=== List of people ===\n${dataLines.joinToString("\n")}")
+    }
+
+
 }
 
